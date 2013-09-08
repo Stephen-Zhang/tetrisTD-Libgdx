37591f3fc70090bc8e6ed5a45aec086e902ce963
Stephen-Zhang <Zhang.Stephen.Y@gmail.com>
2013-09-07 20:22:55 -0400

    merge

--- TetrisTD-New/src/player/Player.java
+++ TetrisTD-New/src/player/Player.java
@@ -9,18 +9,35 @@ import towers.base.BaseTower;
 import towers.base.TowerType;
 import towers.status.AtkSpeedTower;
 
+import com.badlogic.gdx.math.Polygon;
+
 public class Player {
 	public int gold = 0;
 	public int lives;
 	public TowerType holding = TowerType.NULL;
+	
+	public float[] holdingShapeBody = null;
+	public Polygon holdingShape = null;
+	public float[] holdingRange = null;
+	public Polygon holdingRangeBody = null;
+	
 	public float holdingRotation = 0;
+	
 	public ArrayList<TowerType> availTowers = new ArrayList<TowerType>();
 
+	public HashMap<float[], Polygon> threeSqShapes = new HashMap<float[], Polygon>();
+	public HashMap<float[], Polygon> fourSqShapes = new HashMap<float[], Polygon>();
+	public HashMap<float[], Polygon> fiveSqShapes = new HashMap<float[], Polygon>();
+	
 	public int[] currMouseLoc = new int[2];
 	public boolean canPlaceTower = true;
 
 	public Player(int life) {
 		lives = life;
+		
+		populateThrees();
+		populateFours();
+		populateFives();
 	}
 	
 	public Player(int life, int gold) {
@@ -51,7 +68,7 @@ public class Player {
 		switch(holding) {
 		case TEST_TOWER: 
 			return new TestTower(id, this.currMouseLoc, this.holdingRotation);
-		case NULL: 
+		case NULL:
 			return null;
 		case TEST_AOE_TOWER:
 			return new TestAoeTower(id, this.currMouseLoc, this.holdingRotation);
@@ -129,5 +146,166 @@ public class Player {
 			break;
 		}
 		return null;
+	}
+	
+	public void populateThrees() {
+		//Straight
+		float[] straightBody = new float[]{
+			0, 0,
+			0, 1,
+			0, 2
+		};
+		Polygon straight = new Polygon(new float[]{
+			0, 0,
+			32, 0,
+			32, 96,
+			0, 96
+		});
+		
+		//Bent
+		float[] bentBody = new float[]{
+			0, 0,
+			1, 0,
+			0, 1
+		};		
+		Polygon bent = new Polygon(new float[]{
+				0, 0, 
+				64, 0,
+				64, 64,
+				32, 64,
+				32, 32,
+				0, 32,
+		});
+		
+		threeSqShapes.put(straightBody, straight);
+		threeSqShapes.put(bentBody, bent);
+	}
+	
+	public void populateFours() {
+		//Straight
+		float[] straightBody = new float[] {
+			0, 0,
+			0, 1,
+			0, 2,
+			0, 3
+		};
+		Polygon straight = new Polygon(new float[]{
+			0, 0,
+			32, 0,
+			32, 128,
+			0, 128
+		});
+		
+		//Square
+		float[] squareBody = new float[] {
+			0, 0,
+			0, 1,
+			1, 0,
+			1, 1
+		};
+		Polygon square = new Polygon(new float[]{
+			0, 0,
+			0, 64,
+			64, 64,
+			64, 0
+		});
+		
+		//T-Shape
+		float[] tBody = new float[] {
+			0, 0,
+			1, 0,
+			1, 1,
+			2, 0
+		};
+		Polygon t = new Polygon(new float[]{
+			0, 0,
+			0, 32,
+			32, 32,
+			32, 64,
+			64, 64,
+			64, 32,
+			96, 32,
+			96, 0
+		});
+		
+		//L shape
+		float[] lBody = new float[] {
+			0, 0,
+			0, 1,
+			0, 2,
+			1, 0
+		};
+		Polygon l = new Polygon(new float[] {
+			0, 0,
+			0, 96,
+			32, 96,
+			32, 32,
+			64, 32,
+			64, 0
+		});
+		
+		//Reverse L shape
+		float[] revLBody = new float[] {
+			0, 0,
+			1, 0,
+			1, 1,
+			1, 2
+		};
+		Polygon revL = new Polygon(new float[] {
+			0, 0,
+			0, 32,
+			32, 32,
+			32, 96,
+			96, 64,
+			96, 0
+		});
+		
+		//Z shape
+		float[] zBody = new float[] {
+			0, 0,
+			0, 1,
+			1, 1,
+			1, 2
+		};
+		Polygon z = new Polygon(new float[] {
+			0, 0,
+			0, 64,
+			32, 64,
+			32, 96,
+			64, 96,
+			64, 32,
+			32, 32,
+			32, 0			
+		});
+		
+		//S shape
+		float[] sBody = new float[] {
+			0, 0,
+			1, 0,
+			1, 1,
+			2, 1
+		};
+		Polygon s = new Polygon(new float[] {
+			0, 0,
+			0, 32,
+			32, 32,
+			32, 64,
+			96, 64,
+			96, 32,
+			64, 32,
+			64, 0
+		});
+		
+		fourSqShapes.put(sBody, s);
+		fourSqShapes.put(zBody, z);
+		fourSqShapes.put(lBody, l);
+		fourSqShapes.put(revLBody, revL);
+		fourSqShapes.put(straightBody, straight);
+		fourSqShapes.put(squareBody, square);
+		fourSqShapes.put(tBody, t);
+	}
+	
+	public void populateFives() {
+		
 	}
 }
